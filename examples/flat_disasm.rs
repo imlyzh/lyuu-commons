860a076ae96530cca68f09c918099d24ccf80352
imlyzh <enterindex@gmail.com>
2023-02-07 09:26:00 +0800

    fixed example compiler error

--- examples/flat_disasm.rs
+++ examples/flat_disasm.rs
@@ -3,6 +3,6 @@ use lyuu_commons::flat_disasm::disasm::flat_disasm;
 
 fn main() {
   let src = 0b11111111111111111111_00000_0110111_u32.to_le_bytes();
-  let (r, _next) = flat_disasm(&src).unwrap();
+  let (r, _next) = flat_disasm(&src, false).unwrap();
   println!("out: {}", r);
 }
